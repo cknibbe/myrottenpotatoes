01faeba0136534b9d42ba95fe2140566cd7985f4
Clo Knibbe <cknibbe@comcast.net>
2012-03-21 12:27:29 -0400

    added code for book section 5.7

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,11 +2,22 @@ class Movie < ActiveRecord::Base
   
   class Movie::InvalidKeyError < StandardError ; end
   
+  def self.api_key
+    '94f6dee3be15099634c52c2107b03f84'
+  end
+  
    def self.find_in_tmdb(string)
+     Tmdb.api_key = self.api_key
      begin
        TmdbMovie.find(:title => string)
      rescue ArgumentError => tmdb_error
        raise Movie::InvalidKeyError, tmdb_error.message
+     rescue RuntimeError => tmdb_error
+       if tmdb_error.message =~ /status code '404'/
+         raise Movie::InvalidKeyError, tmdb_error.message
+       else
+         raise RuntimeError, tmdb_error.message
+       end
      end
    end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -8,6 +8,13 @@ describe Movie do
       Movie.find_in_tmdb('Inception')
     end
     it 'should raise an InvalidKeyError with no API key' do
+      Movie.stub(:api_key).and_return('')
+      lambda { Movie.find_in_tmdb('Inception') }.
+        should raise_error(Movie::InvalidKeyError)
+    end
+    it 'should raise an InvalidKeyError with invalid API key' do
+      TmdbMovie.stub(:find).
+        and_raise(RuntimeError.new("API status code '404'"))
       lambda { Movie.find_in_tmdb('Inception') }.
         should raise_error(Movie::InvalidKeyError)
     end
