0fa9e88f19c7d7c384a119ba13ace0a9680de09b
Clo Knibbe <cknibbe@comcast.net>
2012-03-21 12:53:31 -0400

    movie_spec.rb: added 'context'

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,21 +2,25 @@ require 'spec_helper'
 
 describe Movie do
   describe 'searching Tmdb by keyword' do
-    it 'should call Tmdb with title keywords given valid API key' do
-      TmdbMovie.should_receive(:find).
-        with(hash_including :title => 'Inception')
-      Movie.find_in_tmdb('Inception')
+    context 'with valid API key' do
+      it 'should call Tmdb with title keywords' do
+        TmdbMovie.should_receive(:find).
+          with(hash_including :title => 'Inception')
+        Movie.find_in_tmdb('Inception')
+      end
     end
-    it 'should raise an InvalidKeyError with no API key' do
-      Movie.stub(:api_key).and_return('')
-      lambda { Movie.find_in_tmdb('Inception') }.
-        should raise_error(Movie::InvalidKeyError)
-    end
-    it 'should raise an InvalidKeyError with invalid API key' do
-      TmdbMovie.stub(:find).
-        and_raise(RuntimeError.new("API status code '404'"))
-      lambda { Movie.find_in_tmdb('Inception') }.
-        should raise_error(Movie::InvalidKeyError)
+    context 'with invalid API key' do
+      it 'should raise an InvalidKeyError with no API key' do
+        Movie.stub(:api_key).and_return('')
+        lambda { Movie.find_in_tmdb('Inception') }.
+          should raise_error(Movie::InvalidKeyError)
+      end
+      it 'should raise an InvalidKeyError with invalid API key' do
+        TmdbMovie.stub(:find).
+          and_raise(RuntimeError.new("API status code '404'"))
+        lambda { Movie.find_in_tmdb('Inception') }.
+          should raise_error(Movie::InvalidKeyError)
+      end
     end
   end
 end
